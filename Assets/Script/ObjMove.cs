6e9c1ac0524988877f246fa046496971f9d751d5
MarcoKuo0132 <m0935008715@gmail.com>
2020-05-11 14:59:48 +0800

    update

---
+++ Assets/Script/ObjMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjMove : MonoBehaviour
+{
+    private float timer;
+    private Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        Vector3 rPos = pos;
+        rPos.x += Mathf.Sin(timer) * 200;
+
+        transform.position = rPos;
+    }
+}
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private TextMesh textMesh;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.text = Mathf.CeilToInt(time).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        textMesh.text = Mathf.CeilToInt(time).ToString();
+
+        if (time <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
